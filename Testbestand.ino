f58bf16037bc094fb079d16155461ec10e344cf4
Deefgame <davidvanurk@outlook.com>
2021-10-15 10:25:53 +0200

    Create Testbestand.ino

---
+++ Testbestand.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+Servo myServo;
+
+int ServoPin = 7;
+int ServoPos = 180;
+
+void setup() {
+  myServo.attach(ServoPin);
+}
+
+void loop() {
+  myServo.write(180);
+  delay(500);
+  myServo.write(0);
+  delay(500);
+}
