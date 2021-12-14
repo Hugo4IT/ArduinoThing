2aaa26f1187502e79514b0e3a6de87a38f980827
Hugo van de Kuilen <hugo.vandekuilen1234567890@gmail.com>
2021-12-14 18:41:04 +0100

    Gonna clean up code on my PC

--- thonk/thonk.ino
+++ thonk/thonk.ino
@@ -1,10 +1,47 @@
-void setup() {
+unsigned char pixelData;
+
+void setup()
+{
   Serial.begin(115200);
+  pinMode(44, INPUT);
+  pinMode(45, OUTPUT);
+  for (int i = 46; i < 53; i++)
+  {
+    pinMode(i, INPUT);
+  }
 }
 
-void loop() {
-  if (Serial.available() > 0) {
-    Serial.print("Arduino received: ");
-    Serial.println((char)Serial.read());
+void readPixelData()
+{
+  pixelData = 0;
+  for (int i = 0; i < 8; i++)
+  {
+    pixelData |= digitalRead(46) << i;
   }
+}
+
+void printData()
+{
+  Serial.print("Data: ");
+  for (int i = 46; i < 53; i++)
+  {
+    Serial.print(digitalRead(i));
+  }
+  Serial.print(" | ");
+  Serial.print(pixelData);
+  Serial.println();
+}
+
+void loop()
+{
+  digitalWrite(45, HIGH);
+  if (digitalRead(44) > 0)
+  {
+    Serial.println("Received pixel clock signal");
+    readPixelData();
+    printData();
+  }
+  digitalWrite(45, LOW);
+
+  delay(3000);
 }
