797d4738e46c8b2cf0fca9818bc645b5b48f460a
Deefgame <davidvanurk@outlook.com>
2022-01-13 11:49:17 +0100

    webserver op school

--- espwifi/espWiFi.ino
+++ espwifi/espWiFi.ino
@@ -1,11 +1,24 @@
-#include <ESP8266Wifi.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid = "iPhone van David";
+const char* password = "ProtonVPN";
 
 void setup () {
     Serial.begin(115200);
     Serial.println();
 
-    WiFi.softAP("test", "arduinothing", 1, false, 1)
-
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    
     Serial.print("Connecting...");
+}
 
+void loop() {
+    Serial.print("IpAddress: ");
+    Serial.println(WiFi.localIP());
+    delay(2000);
 }
