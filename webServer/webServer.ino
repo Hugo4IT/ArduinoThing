774beca5bd15dbfbb3516f1410430dd87412f4a4
Deefgame <davidvanurk@outlook.com>
2022-01-14 11:53:19 +0100

    websockets

--- webServer/webServer.ino
+++ webServer/webServer.ino
@@ -1,1 +1,13 @@
-#include <ESP8266Wifi.h>
+void setup() {
+  Serial.begin(115200);
+  delay(2000);
+  sendData("AT+RST\r\n",2000,DEBUG); // reset module
+  sendData("AT+CWMODE=2\r\n",1000,DEBUG); // configure as access point
+  sendData("AT+CIFSR\r\n",1000,DEBUG); // get ip address
+  sendData("AT+CIPMUX=1\r\n",1000,DEBUG); // configure for multiple connections
+  sendData("AT+CIPSERVER=1,80\r\n",1000,DEBUG); // turn on server on port 80
+}
+
+void loop() {
+  
+}
